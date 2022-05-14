4091f3f91000cb8ca050c5c7d7ec4e0dff6b83fb
Alknight17 <knightalex17@gmail.com>
2022-05-14 13:40:31 -0400

    create POST/DELETE routes, write file to db.json

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,12 @@ module.exports = app => {
         if (err) throw err;
         var notes = JSON.parse(data);
 
-        // create get route for api/notes
+        // create GET route for api/notes
         app.get('/api/notes', function(req, res) {
             res.json(notes);
         });
 
-        // create get route for one specific note
+        // create GET route for one specific note
         app.get('/api/notes/:id', function(req, res) {
             res.json(notes[req.params.id]);
         });
@@ -28,10 +28,27 @@ module.exports = app => {
             res.senfFile(path.join(__dirname, "../public/index.html"));
         });
 
-        
+        // create route to POST api/notes
+        app.post('api/notes', function(req, res) {
+            let newNote = req.body;
+            notes.push(newNote);
+            updateDb();
+            returnconsole.log('Added new note: ' + newNote.title);
+        });
 
-    })
+        // create route to DELETE specific note
+        app.delete('/api/notes/:id', function(req, res) {
+            notes.splice(req.params.id, 1);
+            updateDb();
+            console.log("Note #" + req.params.id + " has been deleted");
+        });
 
-
-
-}
+        // create funtion to update the db.json file when editing/adding/deleting notes
+        function updateDb() {
+            fs.writeFile('db/db.json', JSON.stringify(notes, '\t'), err => {
+                if(err) throw err;
+                return true;
+            });
+        }
+    });
+};
